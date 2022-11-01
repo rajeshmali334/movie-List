6cc201686a3773fbd3d44cad27063238802f2f46
RAJESH <108955671+rajeshmali334@users.noreply.github.com>
2022-11-01 13:15:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "./components/Card";
+import "./App.css";
+import data from "./dataList";
+
+function App() {
+  return (
+    <>
+      <h1 className="heading">Top Movie List</h1>
+      <div className="cards">
+        {data.map((value) => {
+          return (
+            <Card
+              key={value.key}
+              src={value.src}
+              ranking={value.ranking}
+              title={value.title}
+              year={value.year}
+              tv={value.tv}
+              distributor={value.distributor}
+              amount={value.amount}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+export default App;
